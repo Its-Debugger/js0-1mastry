b4e6a4b8da6a9bc4ba09f5b0d275527209ad849b
Its-Debugger <118119257+Its-Debugger@users.noreply.github.com>
2025-04-15 13:30:46 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "t3dclutcher@gmail.com"
+var accountPassword = "1234556"
+accountCity = "kankavli"
+
+// accountId = 2
+accountEmail = "bute@gmail.com"
+accountCity ="Kanedi"
+accountPassword = "212121"
+
+console.log([accountId, accountPassword, accountEmail, accountCity])
+
+// prefer not to use var because of scope problem , functional and block scope
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";//trat all the js as a newer version
+
+// alert("hello");
+// alert(3 + 3) we are using nodejs not browser
+// code redeability should be high
+
+let name = "Sumit"
+let age = 23
+let isOpen = true 
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
